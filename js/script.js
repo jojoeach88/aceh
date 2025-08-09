0a1afd6e9f660c1a318a0cb3be527eed0047ea21
jojoeach88 <zulfitriansyah87@gmail.com>
2025-08-09 23:58:36 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        });
+}
