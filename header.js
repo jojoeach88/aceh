50f4f9c1b00e32d8e2edf989a34b13bd3badcdd7
jojoeach88 <zulfitriansyah87@gmail.com>
2025-08-10 00:14:28 +0700

    Update and rename header.html to header.js

---
+++ header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("components/header.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+            initTranslate();
+        });
+});
