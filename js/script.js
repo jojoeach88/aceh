ff83937b02218d925616db291415a6c7a197383c
jojoeach88 <zulfitriansyah87@gmail.com>
2025-08-10 14:38:34 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-function loadHeader() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-placeholder').innerHTML = data;
-        });
-}
